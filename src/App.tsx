6702b2e925e2d4f2a20d881ef30be62274632676
Sunset-kim <wood2002@naver.com>
2021-12-23 02:53:59 +0900

    react-query  & devtool

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import { ThemeProvider } from 'styled-components';
 import { lightTheme } from './styles/theme';
 import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
+import { QueryClient,QueryClientProvider } from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
-
-
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    return (
+    <QueryClientProvider client={queryClient}>
+      
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+      {/* <ReactQueryDevtools initialIsOpen /> */}
+    </QueryClientProvider>
+    
   );
 }
 
