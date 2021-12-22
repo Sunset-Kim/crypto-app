e655cf07eee8dcc24a47a9102397f9ad88b44677
Sunset-kim <wood2002@naver.com>
2021-12-23 02:56:33 +0900

    📱💄  화면 너비 조정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,14 +8,22 @@ ${reset}
   box-sizing: border-box;
 }
 
-html, #root {
+html {
   height: 100%;
 }
+
 body {
   font-family: 'Lato', sans-serif;
   height: 100%;
   color: ${(props) => props.theme.color.foreground};
   background-color: ${(props) => props.theme.color.background};
+}
+
+#root {
+  height: 100%;
+  max-width: 1024px;
+  min-width: 320px;
+  margin: 0 auto;
 }
 
 
