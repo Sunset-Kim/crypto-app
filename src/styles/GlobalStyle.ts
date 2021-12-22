0d4375d6800e204f85f159733b14aae9f71e75d9
Sunset-kim <wood2002@naver.com>
2021-12-23 01:42:45 +0900

    💄 global style : body 높이값 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,12 +1,23 @@
 import { createGlobalStyle } from "styled-components";
+
 import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
 ${reset}
 
+* {
+  box-sizing: border-box;
+}
+
+html, #root {
+  height: 100%;
+}
 body {
   font-family: 'Lato', sans-serif;
-  color: red;
+  height: 100%;
+  color: ${(props) => props.theme.color.foreground};
+  background-color: ${(props) => props.theme.color.background};
 }
+
 
 a {
   color: inherit;
