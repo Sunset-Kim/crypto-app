68bb607d6acb4fa49a36c6aa2eb2c753aa551568
Sunset-kim <wood2002@naver.com>
2021-12-24 02:46:34 +0900

    🚀  배포 완료 버전 v 1.0.0

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,7 +7,7 @@ import Price from './routes/Price';
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/coin/:coinID" element={<Coin />}>
